7c53f9c18a9383a9c1425e67cff365416f1ec820
侑汰 林 <220311@jc-21.jp>
2024-01-19 10:11:22 +0900

    buffには入ってるっぽい

--- BSSTCP_File_server.cpp
+++ BSSTCP_File_server.cpp
@@ -17,6 +17,7 @@ int listenSock;
 
 // クライアントとの通信用ソケットvector ... クラス化してメンバー変数にしたいなー
 std::vector<int> socks;	// 靴下っぽい
+//std::string recevedFile;
 
 bool Init();
 bool Socket(int sockType);
@@ -71,16 +72,19 @@ int main()
         for (auto sock : socks)
         {
             char buff[MESSAGELENGTH];
+            
             ret = Recv(sock, buff);
+            
             if (ret == 0)
             {
-                std::cout << "Recv message:" << buff << std::endl;
+                
+                //std::cout << "buff = " << buff << std::endl;
             }
             else if (ret != WSAEWOULDBLOCK)
             {
                 std::cout << "Error: Recv()" << std::endl;
             }
-
+            //buff[ret] = '\0';
 
         }
     }
@@ -157,17 +161,29 @@ int Accept()
 // 文字列送信
 int Recv(int sock, char* buff)
 {
-    std::ofstream receivedImage(buff, std::ios::binary);
-    int byteread;
-    int ret = recv(sock, buff, MESSAGELENGTH, 0);
+   // std::ofstream receivedImage(buff, std::ios::binary);
+    //int byteread;
+    //int ret = recv(sock, buff, MESSAGELENGTH, 0);
 
-    while ((byteread = ret) > 0)
+    //while ((byteread = ret) > 0)
+    //{
+    //    receivedImage.write(buff, byteread);
+    //}
+    
+    
+    //int ret = recv(sock, buff, MESSAGELENGTH, 0);//文字受け取るだけ
+    std::ofstream receivedfile(buff, std::ios::binary);
+    int ret = recv(sock, buff, MESSAGELENGTH, 0);//テキストファイル
+    while (ret > 0)
     {
-        receivedImage.write(buff, byteread);
+        receivedfile.write(buff, ret);
     }
-
+    if (ret == MESSAGELENGTH)
+        return 0;
 
     return WSAGetLastError();
+
+
 }
 
 // 終了処理
